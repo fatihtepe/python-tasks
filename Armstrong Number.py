ff53f9e6bd0b4d04a0b4be8d73d238e3ec456c23
fatihtepe <fatihtepe@gmail.com>
2021-04-28 17:21:43 -0400

    second solution

--- Armstrong Number.py
+++ Armstrong Number.py
@@ -33,3 +33,20 @@ else:
 
 
 
+# Second Solution:
+
+while True :
+    number = input("enter a positive number.")
+    digits = len(number)
+    summ = 0
+    if not number.isdigit() :
+        print(number, "is invalid entry. enter numberic value!")
+    elif int(number) >= 0 :
+        for i in range(digits) :
+            summ = summ + int(number[i]) ** digits
+        if summ == int(number) :
+            print(number, "is an Armstrong Number.")
+            break
+        else :
+            print(number, "is not an Armstrong Number.")
+            break
