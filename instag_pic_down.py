fcee3429974174c3233c4822985bb0d4c2ac5518
fatihtepe <fatihtepe@gmail.com>
2021-05-31 19:50:46 -0400

    Instagram Download

--- instag_pic_down.py
+++ instag_pic_down.py
@@ -5,8 +5,9 @@ from Instagram. Its as easy as 3 lines of code !'''
 import instaloader
 
 loader = instaloader.Instaloader(download_videos=False, save_metadata=False, post_metadata_txt_pattern='')
-loader.download_hashtag('DevOps', max_count=10)
+loader.download_hashtag ('Turkey', max_count=10)
 
 '''This code will create a new folder with name #DevOps
 and will have 10 latest images with #DevOps.'''
 # !! pip3 install instaloader
+
